3d2c24a89afa8a9f941920d5646bcdf96280a9da
wu58430 <wu58430@126.com>
2023-12-04 23:24:36 +0800

    MDC COMPLETED

--- version.c
+++ version.c
@@ -4,12 +4,12 @@
 #ifdef GIT_HASH
 	#define VER     GIT_HASH
 #else
-	#define VER     "103"
+	#define VER     "104"
 #endif
 
 #ifndef ONE_OF_ELEVEN_VER
 	const char Version[]      = "OEFW-"VER;
 	const char UART_Version[] = "UV-K5 Firmware, Open Edition, OEFW-"VER"\r\n";
 #else
-    const char Version[]      = "L"VER;
+    const char Version[]      = VER;
 #endif
