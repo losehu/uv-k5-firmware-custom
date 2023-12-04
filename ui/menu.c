85b2655d02a009c2b8eae3c0007e2e208a4162cc
wu58430 <wu58430@126.com>
2023-12-04 21:42:57 +0800

    MDC UI

--- ui/menu.c
+++ ui/menu.c
@@ -689,12 +689,12 @@ void UI_DisplayMenu(void) {
             if (!gIsInSubMenu||edit_index<0) {    // show the channel name
                                //     sprintf(edit, "%04lX", gEeprom.MDC1200_ID); // %04X确保输出是4个字符长度的十六进制数
 
-                //    sprintf(String, "%04X", gEeprom.MDC1200_ID); // %04X确保输出是4个字符长度的十六进制数
+                    sprintf(String, "%04X", gEeprom.MDC1200_ID); // %04X确保输出是4个字符长度的十六进制数
                 UI_PrintStringSmall(String, menu_item_x1, menu_item_x2, 3);//4
             } else {    // show the channel name being edited
                 UI_PrintStringSmall(edit, menu_item_x1, menu_item_x2, 3);
                 if (edit_index < 4)
-                    UI_PrintStringSmall("^", menu_item_x1 + (8 * edit_index), menu_item_x2, 4);  // show the cursor
+                    UI_PrintStringSmall("^", (((menu_item_x2 - menu_item_x1) - (32)) + 1) / 2 + (8 * edit_index), menu_item_x2, 4);  // show the cursor
             }
             already_printed = true;
             break;
