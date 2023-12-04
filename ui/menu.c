1cef3afd943f898183082dfc1d075779081699ff
wu58430 <wu58430@126.com>
2023-12-04 21:21:54 +0800

    MDC RX

--- ui/menu.c
+++ ui/menu.c
@@ -687,6 +687,7 @@ void UI_DisplayMenu(void) {
         {
 //            gIsInSubMenu
             if (!gIsInSubMenu||edit_index<0) {    // show the channel name
+                                    sprintf(edit, "%04lX", gEeprom.MDC1200_ID); // %04X确保输出是4个字符长度的十六进制数
 
                     sprintf(String, "%04X", gEeprom.MDC1200_ID); // %04X确保输出是4个字符长度的十六进制数
                 UI_PrintStringSmall(String, menu_item_x1, menu_item_x2, 3);//4
