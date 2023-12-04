34bcbac2dfcc94277350da092037f472dc6147ac
wu58430 <wu58430@126.com>
2023-12-04 21:16:46 +0800

    MDC RX

--- app/menu.c
+++ app/menu.c
@@ -13,6 +13,7 @@
  *     See the License for the specific language governing permissions and
  *     limitations under the License.
  */
+#include <stdio.h>
 #include "app/mdc1200.h"
 #include <string.h>
 #include "driver/uart.h"
