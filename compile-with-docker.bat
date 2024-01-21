96127f9dd993cbe0780a5904d5e8703fd4f7271b
wu58430 <wu58430@126.com>
2024-01-21 14:08:39 +0800

    IDE

--- compile-with-docker.bat
+++ compile-with-docker.bat
@@ -1,4 +1,4 @@
 @echo on
 docker build -t uvk5 .
-docker run --rm -v %CD%/compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make build && cp *.bin compiled-firmware/"
+docker run --rm -v %CD%/compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make both && cp *.bin compiled-firmware/"
 pause
