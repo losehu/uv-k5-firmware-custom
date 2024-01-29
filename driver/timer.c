cf8d60e2425d35cd057b9555d3b01c077f4a60f2
wu58430 <wu58430@126.com>
2024-01-29 22:32:46 +0800

    使能NVIC

--- driver/timer.c
+++ driver/timer.c
@@ -12,15 +12,15 @@ void TIM0_INIT()
 
     TIMERBASE0_DIV=48000;
     TIMERBASE0_LOW_LOAD=10000;
-    TIMERBASE0_HIGH_LOAD=3000;
 
     // Enable TIMERBASE0
     NVIC_SetPriority (Interrupt5_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL); /* set Priority for Systick Interrupt */
 
-    TIMERBASE0_IF = 0x03 ; // 写1清零 清除定时器中断状态
-    TIMERBASE0_IE|= 0x03;
+    TIMERBASE0_IF = 0x01 ; // 写1清零 清除定时器中断状态
+    TIMERBASE0_IE|= 0x01;
 
-    TIMERBASE0_EN|= 0x03;
+    TIMERBASE0_EN|= 0x01;
+    NVIC_EnableIRQ(Interrupt5_IRQn);
     __enable_irq(); // Function to enable interrupts
 
 
@@ -31,9 +31,9 @@ void TIM0Handler(void);
 
 void TIM0Handler(void)
 {
-    TIM0_CNT++;
-    TIMERBASE0_IF =0; // 写1清零 清除定时器中断状态
+    TIMERBASE0_IF = 0x01 ;
 
+    TIM0_CNT++;
 }
 
 
