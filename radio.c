71938cbe19f2dec236c69756255d264f2d739276
wu58430 <wu58430@126.com>
2024-01-30 12:16:59 +0800

    STE 关闭时不发送尾音

--- radio.c
+++ radio.c
@@ -61,7 +61,8 @@ void RADIO_SendEndOfTransmission(void)
     DTMF_SendEndOfTransmission();
 
     // send the CTCSS/DCS tail tone - allows the receivers to mute the usual FM squelch tail/crash
-    RADIO_EnableCxCSS();
+    if(gEeprom.TAIL_TONE_ELIMINATION)
+        RADIO_SendCssTail();
     RADIO_SetupRegisters(false);
 }
 
@@ -1064,7 +1065,7 @@ void RADIO_PrepareTX(void)
 #endif
 }
 
-void RADIO_EnableCxCSS(void)
+void RADIO_SendCssTail(void)
 {
     switch (gCurrentVfo->pTX->CodeType) {
         case CODE_TYPE_DIGITAL:
@@ -1085,6 +1086,7 @@ void RADIO_PrepareCssTX(void)
 
     SYSTEM_DelayMs(200);
 
-    RADIO_EnableCxCSS();
+    if(gEeprom.TAIL_TONE_ELIMINATION)
+        RADIO_SendCssTail();
     RADIO_SetupRegisters(true);
 }
--- radio.h
+++ radio.h
@@ -163,7 +163,7 @@ void     RADIO_SetTxParameters(void);
 void     RADIO_SetModulation(ModulationMode_t modulation);
 void     RADIO_SetVfoState(VfoState_t State);
 void     RADIO_PrepareTX(void);
-void     RADIO_EnableCxCSS(void);
+void     RADIO_SendCssTail(void);
 void     RADIO_PrepareCssTX(void);
 void     RADIO_SendEndOfTransmission(void);
 void RADIO_SetupAGC(bool listeningAM, bool disable);
