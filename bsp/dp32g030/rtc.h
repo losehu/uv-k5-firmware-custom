302cb93dc1c7a9e9f9f28ae6193054280b26e614
wu58430 <wu58430@126.com>
2024-01-30 12:01:49 +0800

    增加RTC测试

---
+++ bsp/dp32g030/rtc.h
@@ -0,0 +1,36 @@
+//
+// Created by RUPC on 2024/1/30.
+//
+#ifndef HARDWARE_DP32G030_RTC_H
+#define HARDWARE_DP32G030_RTC_H
+#include <stdint.h>
+
+//RCLF 32768HZ
+#define RTC_BASE_ADD 0x40069000
+#define RTC_CFG_ADD (0x00+RTC_BASE_ADD) //配置寄存器
+#define RTC_IE_ADD (0x04+RTC_BASE_ADD) //中断使能寄存器
+#define RTC_IF_ADD (0x08+RTC_BASE_ADD)// 状态寄存器
+#define RTC_PRE_ADD (0x10+RTC_BASE_ADD) // 预分频寄存器
+#define RTC_TR_ADD (0x14+RTC_BASE_ADD) //时间寄存器
+#define RTC_DR_ADD (0x18+RTC_BASE_ADD) // 日期寄存器
+#define RTC_AR_ADD (0x1C+RTC_BASE_ADD) //闹钟寄存器
+#define RTC_TSTR_ADD (0x20+RTC_BASE_ADD) //当前时间寄存器
+#define RTC_TSDR_ADD (0x24+RTC_BASE_ADD) // 当前日期寄存器
+#define RTC_CNT_ADD (0x28+RTC_BASE_ADD) // 秒标当前计数值
+#define RTC_VALID_ADD (0x2C+RTC_BASE_ADD) //当前时间有效标志寄存器
+
+#define RTC_CFG (*(volatile uint32_t *)RTC_CFG_ADD) //配置寄存器
+#define RTC_IE (*(volatile uint32_t *)RTC_IE_ADD) //中断使能寄存器
+#define RTC_IF (*(volatile uint32_t *)RTC_IF_ADD)// 状态寄存器
+#define RTC_PRE (*(volatile uint32_t *)RTC_PRE_ADD) // 预分频寄存器
+#define RTC_TR (*(volatile uint32_t *)RTC_TR_ADD) //时间寄存器
+#define RTC_DR (*(volatile uint32_t *)RTC_DR_ADD) // 日期寄存器
+#define RTC_AR (*(volatile uint32_t *)RTC_AR_ADD) //闹钟寄存器
+#define RTC_TSTR (*(volatile uint32_t *)RTC_TSTR_ADD) //当前时间寄存器
+#define RTC_TSDR (*(volatile uint32_t *)RTC_TSDR_ADD) // 当前日期寄存器
+#define RTC_CNT (*(volatile uint32_t *)RTC_CNT_ADD) // 秒标当前计数值
+#define RTC_VALID (*(volatile uint32_t *)RTC_VALID_ADD) //当前时间有效标志寄存器
+void RTC_INIT();
+
+
+#endif
---
+++ driver/rtc.c
@@ -0,0 +1,25 @@
+//
+// Created by RUPC on 2024/1/30.
+//
+#include "bsp/dp32g030/rtc.h"
+
+void RTC_INIT() {
+
+    RTC_PRE |= (32768 - 1)//PRE_ROUND=32768HZ-1
+               | (0 << 20)//DECIMAL=0
+               | (0 << 24);//PRE_PERIOD=8s
+    RTC_TR = (2 << 24)//day 2
+             |(1<<20) //h十位
+             |(1<<16)//h个位
+             |(4<<12)//min十位
+             |(7<<8)//min个位
+              |(3<<4)//sec十位
+              |(0<<0);//sec个位
+    RTC_DR = (2 << 20)//YEAR TEN
+             |(4<<16) //YEAR ONE
+             |(0<<12)//MONTH TEN
+             |(1<<8)//MONTH ONE
+             |(3<<4)//DAY TEN
+             |(0<<0);//DAY ONE
+
+}
