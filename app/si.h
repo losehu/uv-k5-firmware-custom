225538d75c9006ddd1626b13fa02060cb2db197e
wu58430 <wu58430@126.com>
2024-05-23 21:28:17 +0800

    fix build error

--- app/si.h
+++ app/si.h
@@ -1,5 +1,6 @@
 #ifndef APP_SI_H
 #define APP_SI_H
+#include "spectrum.h"
 
 #include "../driver/keyboard.h"
 #include <stdbool.h>
@@ -10,5 +11,6 @@ void SI_update();
 bool SI_key(KEY_Code_t key, bool bKeyPressed, bool bKeyHeld);
 void SI_render();
 void SI_deinit();
-
+void Key_FM(KeyboardState kbds) ;
+    void HandleUserInput() ;
 #endif /* end of include guard: APP_SI_H */
