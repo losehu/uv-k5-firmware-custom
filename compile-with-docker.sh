04e1defb4ccc0c5cade3cfb36810da4263163e25
RUBO <52628935+losehu@users.noreply.github.com>
2024-07-05 21:47:08 +0800

    Update compile-with-docker.sh

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -2,4 +2,4 @@
 rm -rf compiled
 make clean
 docker build -t uvk5 .
-docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app &&rm -rf compiled&& make clean && make && cp *.bin compiled-firmware/"
+docker run --rm -v "$(pwd)/compiled-firmware:/app/compiled-firmware" uvk5 /bin/bash -c "cd /app && rm -rf compiled && make clean && make && cp *.bin compiled-firmware/"
