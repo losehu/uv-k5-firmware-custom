38c13b72f4e7716f1ed455cf641532f2bb1cf81c
losehu <wu58430@126.com>
2026-09-20 16:31:22 +0800

    release: prepare Doppler version 0.3

--- del_linux.sh
+++ del_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 target_directory="./"
-find "$target_directory" -type f \( -name "*.o" -o -name "*.d" \)
+find "$target_directory" -type f \( -name "*.o" -o -name "*.d" \) -delete
 echo "Deletion complete."
